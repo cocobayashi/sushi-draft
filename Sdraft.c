1f78e6ba1d924dcf2d5dca62f2d571f64a3f9c4f
cocobayashi <ta9ya.ktl2@gmail.com>
2021-05-29 20:10:39 +0900

    バグ修正

--- Sdraft.c
+++ Sdraft.c
@@ -2,21 +2,19 @@
 #include <stdlib.h>
 
 int main(void)
-{
-    int Mypack[15];                       //神話 2 種類 レア 5 種類 アンコモン 7 種類 コモン 9 種類
-    int pack[15][4];
-    int cade[249];
-    int i;
-    int j;
-    int k;
-    int temp;
-    int temp1;
-    int scan;
-    int flag;
-    int Mysushi[15];
+{          
+    int Mypack[15];
+    int pack[15][5];                        //パック 15はパックの中身数を表す  1~4はドラフトパック 5は自分がpickしたパック 
+    int i;                                  //ループカウンタ
+    int j;                                  //ループカウンタ
+    int k;                                  //ループカウンタ
+    int temp;                               //作業領域
+    int temp1;                              //作業領域１
+    int scan;                               //入力
+    int flag;                               //フラグ
 
-    for (i = 0; i < 4; i++) {                           //寿司パックを計４パック作成
-        for (j = 0; j < 15; j++) {                     //寿司パック作成
+    for (i = 0; i < 4; i++) {                           //寿司パックを計４パック作成   神話 2 種類 レア 5 種類 アンコモン 7 種類 コモン 9 種類
+        for (j = 0; j < 15; j++) {                      //寿司パック作成ループ
             do {                                    
                 flag = 0;
                 if (j == 0) {
@@ -34,16 +32,18 @@ int main(void)
                     }
                 }       
             } while (flag == 1);                        //チェック
-            pack[j][i] = temp;                             //パックにネタの情報を入れる
+
+            pack[j][i] = temp;                             //パックにネタの情報を入れる      
         }
     }
 
 
-    for (i = 0; i < 15; i++) {
-        printf(" %d pick目\n", i + 1);
-        temp = i % 4;
-        for (j = 0; j < 15; j++) {
-            if (pack[j][temp] != -1){
+    for (i = 0; i < 15; i++) {                          //pick(１５回)   
+            printf(" %d pick目\n", i + 1);
+            temp = i % 4;
+
+        for (j = 0; j < 15; j++) {                      //pickするパック内を表示
+            if (pack[j][temp] != -1){                   //パック内に存在しないのは表示しない
                 printf("%d ",j + 1);
                 switch (pack[j][temp]) {
                     case 0:
@@ -123,11 +123,13 @@ int main(void)
                 }
             }
         }
+
         printf("\n");
         printf("ネタを選んで下さい。(数字で入力してください。)\n");
         printf("==> ");
         scanf("%d", &scan);
-        do {
+
+        do {                                                    //エラーチェック
             scan -= 1;
             flag = 0;
             if (scan > 14 || scan < 0) {
@@ -146,10 +148,14 @@ int main(void)
                 flag = 1;
             }
         } while (flag == 1);
-        Mysushi[i] = pack[scan][temp];
-        pack[scan][temp] = -1;
+
+        Mypack[i] = pack[scan][temp];
+        pack[scan][temp] = -1;                                  // -1 = パック内が空
 
         for (j = 0; j < 4; j++) {                           //pick中以外のパックからランダムにネタを抜くループ
+            if (i == 14) {
+                break;
+            }
             if (temp == 0 && j == 0) {
 
             } else if (temp == 3 && j == 1) {
@@ -166,12 +172,91 @@ int main(void)
                             flag = 1;
                         }
                 } while (flag == 1);
+
                 pack[temp1][j] = -1;
             }
         }
     }
-    for (i = 0; i < 15; i++) {
-        printf("%d、%d", i, Mysushi[i]);
+
+    printf("あなたがpickしたネタは…\n");
+    for (i = 0; i < 15; i++){
+        printf("%d ",i + 1);
+        switch (Mypack[i]) {
+                    case 0:
+                        printf("大トロ");
+                        break;
+                    case 1:
+                        printf("ウニ&いくら");
+                        break;
+                    case 2:
+                        printf("赤エビ");
+                        break;
+                    case 3:
+                        printf("煮穴子");
+                        break;
+                    case 4:
+                        printf("中とろ");
+                        break;
+                    case 5:
+                        printf("寒ブリ");
+                        break;
+                    case 6:
+                        printf("甘鯛");
+                        break;
+                    case 7:
+                        printf("アジ");
+                        break;
+                    case 8:
+                        printf("マグロ");
+                        break;
+                    case 9:
+                        printf("サーモン");
+                        break;
+                    case 10:
+                        printf("いか");
+                        break;
+                    case 11:
+                        printf("タコ");
+                        break;
+                    case 12:
+                        printf("エビ");
+                        break;
+                    case 13:
+                        printf("ホタテ");
+                        break;
+                    case 14:
+                        printf("かっぱ巻き");
+                        break;
+                    case 15:
+                        printf("かんぴょう巻き");
+                        break;
+                    case 16:
+                        printf("いなり");
+                        break;
+                    case 17:
+                        printf("玉子焼き");
+                        break;
+                    case 18:
+                        printf("ナス");
+                        break;
+                    case 19:
+                        printf("納豆巻き");
+                        break;
+                    case 20:
+                        printf("新香巻き");
+                        break;
+                    case 21:
+                        printf("ハンバーグ");
+                        break;
+                    case 22:
+                        printf("コーン");
+                        break;
+                }   
+                if (j < 14) {
+                    printf("、");
+                } else {
+                    printf("\n");
+                }
     }
     return 0;
 }
